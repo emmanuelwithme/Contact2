2c8274d807d1ed8cdbe3c3f8f0236cc58cb8e0bf
emmanuelwithme <wow90327@gmail.com>
2022-06-16 21:28:35 +0800

    Add Item action button with NavArg first complete

--- app/src/main/java/com/example/contact2/FirstFragment.kt
+++ app/src/main/java/com/example/contact2/FirstFragment.kt
@@ -1,12 +1,13 @@
 package com.example.contact2
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.contact2.databinding.FragmentFirstBinding
+import com.google.android.material.snackbar.Snackbar
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -34,6 +35,21 @@ class FirstFragment : Fragment() {
 
         binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+            val directions = FirstFragmentDirections.actionFirstFragmentToSecondFragment(
+                "Add Contact",
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                0
+            )
+            this.findNavController().navigate(directions)
         }
     }
 
--- app/src/main/java/com/example/contact2/MainActivity.kt
+++ app/src/main/java/com/example/contact2/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.example.contact2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,10 +31,6 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
